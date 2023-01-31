68fbb97350dfa66181bf0e399c177f3202b00d6e
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-01-31 17:33:04 +0000

    chore: generate responsive images on build

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,42 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const pluginRss = require('@11ty/eleventy-plugin-rss')
 
+const Image = require('@11ty/eleventy-img')
+
+const path = require('path')
+const getResolvedSrc = (src, page) => {
+    const isRemote = !src.startsWith('.')
+    if (isRemote) return src
+
+    const isColocated = !isRemote && !src.startsWith('./src')
+    if (!isColocated) return src
+
+    return path.resolve(page.inputPath, '../', src)
+}
+
+async function imageShortcode(
+    src,
+    alt,
+    sizes = '(min-width: 1200px) 60rem, 100vw'
+) {
+    const metadata = await Image(getResolvedSrc(src, this.page), {
+        widths: [600, 1200],
+        formats: ['avif', 'webp', 'jpeg'],
+        outputDir: './src/site/images/opt',
+        urlPath: '/images/opt',
+    })
+
+    const imageAttributes = {
+        alt,
+        sizes,
+        loading: 'lazy',
+        decoding: 'async',
+    }
+
+    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes)
+}
+
 module.exports = function (config) {
     // A useful way to reference the context we are runing eleventy in
     let env = process.env.ELEVENTY_ENV
@@ -35,20 +71,20 @@ module.exports = function (config) {
 
     config.addPlugin(pluginRss)
 
-    config.addPairedShortcode("figure", (content, src, alt) => {
+    config.addPairedShortcode('figure', (content, src, alt) => {
         return `
         <figure class='post__figure'>
             <img src='${src}' alt='${alt}' loading='lazy'>
             <figcaption>${content}</figcaption>
         </figure>
-        `.trim()        
+        `.trim()
     })
 
-    config.addShortcode(
-        "picture",
-        (src, alt) => `<p><img loading='lazy' src='${src}' alt='${alt}'/></p>`.trim()
+    config.addShortcode('picture', (src, alt) =>
+        `<p><img loading='lazy' src='${src}' alt='${alt}'/></p>`.trim()
     )
 
+    config.addAsyncShortcode('pic', imageShortcode)
 
     // make the seed target act like prod
     env = env == 'seed' ? 'prod' : env
